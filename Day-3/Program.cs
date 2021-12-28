1de1a3383f4b4891da4a5bffd4321b5861ddefba
Michael Hoser <michael.hoser@outlook.com>
2021-12-28 15:59:13 +0100

    Refactor Part 1

--- Day-3/Program.cs
+++ Day-3/Program.cs
@@ -1,4 +1,4 @@
-﻿void DumpArray(int[] input)
+﻿void DumpCounter(int[] input)
 {
     for (var i = 0; i < input.Length; i++)
     {
@@ -7,41 +7,42 @@
     }
 }
 
-
-var input = File.ReadAllLines("input.txt");
-
-var num0 = new int[12];
-var num1 = new int[12];
-
-foreach (var line in input)
+string CalcBits(string[] input, bool mostCommon)
 {
-    for (var pos = 0; pos < line.Length; pos++)
+    var num0 = new int[12];
+    var num1 = new int[12];
+
+    foreach (var line in input)
     {
-        if (line[pos] == '1')
-            num1[pos]++;
-        else
-            num0[pos]++;
+        for (var pos = 0; pos < line.Length; pos++)
+        {
+            if (line[pos] == '1') num1[pos]++; else num0[pos]++;
+        }
     }
 
-    Console.Write($"{line}: ");
-    DumpArray(num0);
-    Console.Write("  ");
-    DumpArray(num1);
-    Console.WriteLine();
+    var result = "";
+    for (var digit = 0; digit < num0.Length; digit++)
+    {
+        if (mostCommon)
+            result += (num1[digit] >= num0[digit]) ? '1' : '0';
+        else
+            result += (num1[digit] >= num0[digit]) ? '0' : '1';
+    }
+
+    return result;
 }
 
-var gamma = "";
-var epsilon = "";
-for (var digit = 0; digit < num0.Length; digit++)
-{
-    gamma += (num1[digit] > num0[digit]) ? '1' : '0';
-    epsilon += (num1[digit] > num0[digit]) ? '0' : '1';
+// PART 1
 
-}
+var input = File.ReadAllLines("input.txt");
+var gammaStr = CalcBits(input, true);
+var epsilonStr = CalcBits(input, false);
 
-var g = Convert.ToInt32(gamma, 2);
-var e = Convert.ToInt32(epsilon, 2);
+var gamma = Convert.ToInt32(gammaStr, 2);
+var epsilon = Convert.ToInt32(epsilonStr, 2);
 
-Console.WriteLine($"Gamme Rate: {gamma}: {g}");
-Console.WriteLine($"Epsilon Rate: {epsilon}: {e}");
-Console.WriteLine($"Power consumption: {g * e}");
+Console.WriteLine($"Gamme Rate: {gammaStr}: {gamma}");
+Console.WriteLine($"Epsilon Rate: {epsilonStr}: {epsilon}");
+Console.WriteLine($"Power consumption: {gamma * epsilon}");
+
+// PART 2
