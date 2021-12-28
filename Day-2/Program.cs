df80a805b715774f66e6c2ea6c5dc0322d78e43d
Michael Hoser <michael.hoser@outlook.com>
2021-12-28 12:07:44 +0100

    Day 2 Part 1

---
+++ Day-2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+var h = 0;
+var d = 0;
+
+var input = File.ReadAllLines("input.txt");
+foreach (var line in input)
+{
+    var splitted = line.Split(' ');
+    var amount = Convert.ToInt32(splitted[1]);
+    switch (splitted[0])
+    {
+        case "up": d -= amount; break;
+        case "down": d += amount; break;
+        case "forward": h += amount; break;
+    }
+}
+
+Console.WriteLine($"Horizontal Position: {h}");
+Console.WriteLine($"Depth: {d}");
+Console.WriteLine($"Sum: {h * d}");
